c428ac8ab46290ebb16e9697a3be2f31dff22f9b
jaikaran109 <jaik05581@gmail.com>
2026-06-11 01:31:19 +0530

    Create LC_Q21_MergeTwoSortedLists.java

---
+++ LinkedList-Part-I/Easy/LC_Q21_MergeTwoSortedLists.java
@@ -0,0 +1,24 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode ans = new ListNode(-1);
+        ListNode l3 = ans;
+        while(l1 != null && l2 != null){
+            if(l1.val <= l2.val){
+                l3.next = l1;
+                l1 = l1.next;
+            }
+            else {
+                l3.next = l2;
+                l2 = l2.next;
+            }
+            l3 = l3.next;
+        }
+        if(l2 != null){
+            l3.next = l2;
+        }
+        if(l1 != null){
+            l3.next = l1;
+        }
+        return ans.next;
+    }
+}
