ba5f3a9929e6838a05ca5285fe3257a3e0a73308
jaikaran109 <jaik05581@gmail.com>
2026-06-22 17:25:06 +0530

    Update LC_Q206_ReverseLL.java

--- LinkedList-Part-I/Easy/LC_Q206_ReverseLL.java
+++ LinkedList-Part-I/Easy/LC_Q206_ReverseLL.java
@@ -1,3 +1,4 @@
+// Optimal
 class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
@@ -12,3 +13,42 @@ class Solution {
         return prev;
     }
 }
+
+
+
+// Recursive
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+//        if(head == null || head.next == null) return head; // for 0 or 1 nodes
+
+//        ListNode newHead = reverseList(head.next);
+//        ListNode front = head.next;
+//        front.next = head;
+//        head.next = null;
+//        return newHead;
+//     }
+// }
+
+
+
+
+
+// Brute Force
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+//         Stack<Integer> st = new Stack<>();
+//         ListNode temp = head;
+//         while(temp != null){
+//             st.push(temp.val);
+//             temp = temp.next;
+//         }
+
+//         ListNode dummy = new ListNode(-1);
+//         temp = dummy;
+//         while(!st.isEmpty()){
+//             temp.next = new ListNode(st.pop());
+//             temp = temp.next;
+//         }
+//         return dummy.next;
+//     }
+// }
