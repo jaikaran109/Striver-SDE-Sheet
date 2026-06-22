e03f77fe54b3f791634095384140b993431f7393
jaikaran109 <jaik05581@gmail.com>
2026-06-22 19:35:17 +0530

    Create LC_Q128_SortList.java

---
+++ SomeExtraQues/LinkedList/LC_Q128_SortList.java
@@ -0,0 +1,20 @@
+class Solution {
+    public ListNode sortList(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = head;
+        ArrayList<Integer> list = new ArrayList<>();
+        while(temp != null){
+            list.add(temp.val);
+            temp = temp.next;
+        }
+
+        Collections.sort(list);
+
+        temp = dummy;
+        for(int i = 0 ; i < list.size() ; i++){
+            temp.next = new ListNode(list.get(i));
+            temp = temp.next;
+        }
+        return dummy.next;
+    }
+}
