0dfc01e6459d1d8c047579fe567dbcf3790ef44a
jaikaran109 <jaik05581@gmail.com>
2026-07-01 02:58:14 +0530

    Create LeadersInArray.java

---
+++ SomeExtraQues/Arrays/Easy/LeadersInArray.java
@@ -0,0 +1,26 @@
+class Solution {
+    static ArrayList<Integer> leaders(int arr[]) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int max = arr[arr.length - 1];
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] >= max) {
+                list.add(arr[i]);
+                max = arr[i];
+            }
+        }
+
+        Collections.reverse(list);
+        return list;
+    }
+}
+
+
+
+
+// Brute Force
+// i = 0 - n
+// j = i - n
+// check if a[j] > a[i] false break;
+// else add
