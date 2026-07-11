6d571f540f00f34d70c7cd2e2bb4225942620843
jaikaran109 <jaik05581@gmail.com>
2026-07-12 04:02:19 +0530

    Create LC_Q507_PerfectNumber.java

---
+++ SomeExtraQues/Math/Easy/LC_Q507_PerfectNumber.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if(num <= 0 || num == 2 || num % 2 != 0) return false;  // prime no. and odd numbers are not perfect
+        int original = num;
+
+        int sum = 1;
+
+        for(int i = 2 ; i <= Math.sqrt(num) ; i++){
+            if(num % i == 0){
+                sum += i;
+                sum += original / i;
+                original = num;
+            }
+        }
+        return sum == num ;
+    }
+}
+
+
+// Brute Force
+// class Solution {
+//     public boolean checkPerfectNumber(int num) {
+//         int sum = 0;
+//         for(int i = 1 ; i < num ; i++){
+//             if(num % i == 0) sum += i;
+//         }
+//         return sum == num;
+//     }
+// }
