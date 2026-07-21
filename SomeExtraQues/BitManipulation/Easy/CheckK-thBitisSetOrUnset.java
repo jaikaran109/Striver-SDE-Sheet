32cb16c63a378f673ba024e3e04acf69b390a9f6
jaikaran109 <jaik05581@gmail.com>
2026-07-21 20:04:47 +0530

    Create CheckK-thBitisSetOrUnset.java

---
+++ SomeExtraQues/BitManipulation/Easy/CheckK-thBitisSetOrUnset.java
@@ -0,0 +1,12 @@
+// GFG
+
+class CheckBit {
+    static boolean checkKthBit(int n, int k) {
+        return (n & (1 << k)) != 0;  // jitna k ki value h utna 1 ko left shift kr do aur phir and lo
+        
+        // ex - n = 4 , k = 0
+        // 4 - 100
+        // 1 << 0 - 001 
+        // and is 0 , so ans is false
+    }
+}
