233d0df2a9ee088fcd7a65338d61a96cd93e1004
jaikaran109 <jaik05581@gmail.com>
2026-07-21 22:32:48 +0530

    Create SetK-thBit.java

---
+++ SomeExtraQues/BitManipulation/Easy/SetK-thBit.java
@@ -0,0 +1,8 @@
+class Solution {
+    static int setKthBit(int n, int k) {
+        return n | (1 << k);
+        
+        // 1 ko k bit shift kr ke jb tm OR loge to jo bhi number hoga vo 1 set ho jayega
+        
+    }
+}
