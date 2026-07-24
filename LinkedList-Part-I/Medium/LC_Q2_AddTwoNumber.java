30851f106270673fd9a7cba8892181798e45044a
jaikaran109 <jaik05581@gmail.com>
2026-07-24 22:04:54 +0530

    Update LC_Q2_AddTwoNumber.java

--- LinkedList-Part-I/Medium/LC_Q2_AddTwoNumber.java
+++ LinkedList-Part-I/Medium/LC_Q2_AddTwoNumber.java
@@ -1,6 +1,6 @@
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode ans = new ListNode(-1)        ;
+        ListNode ans = new ListNode(-1);
         ListNode temp = ans;
 
         int carry = 0;
