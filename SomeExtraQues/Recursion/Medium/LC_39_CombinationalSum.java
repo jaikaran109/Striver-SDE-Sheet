33428cb7f9af3fc6a4c048237ef7d7eeffd10eec
jaikaran109 <jaik05581@gmail.com>
2026-08-25 03:26:24 +0530

    Update LC_39_CombinationalSum.java

--- SomeExtraQues/Recursion/Medium/LC_39_CombinationalSum.java
+++ SomeExtraQues/Recursion/Medium/LC_39_CombinationalSum.java
@@ -1,32 +1,59 @@
 class Solution {
 
-    static List<List<Integer>> subsequences(int[] nums , int target , int idx , int currSum , List<Integer> ans){
-        List<List<Integer>> temp ;
-        if(currSum == target){
-            temp = new ArrayList<>();
-            temp.add(new ArrayList<>(ans));     //ans ek hi mutable ArrayList hai aur backtracking mein baar-baar change ho raha hai. Isliye result mein direct reference store nahi karna chahiye.
-
-            return temp;
+    private void sum(int idx ,int[] nums , int target ,List<List<Integer>> ans , List<Integer> ds){
+        if(idx == nums.length){
+            if(target == 0){
+                ans.add(new ArrayList<>(ds));
+            }
+            return;
         }
-        
-        if(idx == nums.length || currSum > target) {   // array Finish or sum exceeded
-            return new ArrayList<>(); 
+        if(nums[idx] <= target){
+            ds.add(nums[idx]);
+            sum(idx,nums,target - nums[idx],ans,ds);
+            ds.remove(ds.size() - 1);
         }
-
-        ans.add(nums[idx]); // add current element 
- 
-        List<List<Integer>> left = subsequences(nums,target,idx,currSum+nums[idx],ans);
-
-        ans.remove(ans.size() - 1); // Backtrack
-
-        List<List<Integer>> right = subsequences(nums,target,idx+1,currSum,ans);
-
-        left.addAll(right);
-        return left;
-
+        sum(idx+1,nums,target,ans,ds); 
     }
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        return subsequences(candidates,target,0,0,new ArrayList<>());
+        List<List<Integer>> ans = new ArrayList<>();
+        sum(0,candidates,target,ans,new ArrayList<>());
+        return ans;
     }
 }
+
+
+
+
+// class Solution {
+
+//     static List<List<Integer>> subsequences(int[] nums , int target , int idx , int currSum , List<Integer> ans){
+//         List<List<Integer>> temp ;
+//         if(currSum == target){
+//             temp = new ArrayList<>();
+//             temp.add(new ArrayList<>(ans));     //ans ek hi mutable ArrayList hai aur backtracking mein baar-baar change ho raha hai. Isliye result mein direct reference store nahi karna chahiye.
+
+//             return temp;
+//         }
+        
+//         if(idx == nums.length || currSum > target) {   // array Finish or sum exceeded
+//             return new ArrayList<>(); 
+//         }
+
+//         ans.add(nums[idx]); // add current element 
+
+//         List<List<Integer>> left = subsequences(nums,target,idx,currSum+nums[idx],ans);
+
+//         ans.remove(ans.size() - 1); // Backtrack
+
+//         List<List<Integer>> right = subsequences(nums,target,idx+1,currSum,ans);
+
+//         left.addAll(right);
+//         return left;
+
+//     }
+
+//     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+//         return subsequences(candidates,target,0,0,new ArrayList<>());
+//     }
+// }
