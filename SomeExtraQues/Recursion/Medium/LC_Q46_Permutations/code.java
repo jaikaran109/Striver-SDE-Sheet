093e415045c508f60c714e62aa17dbb4969b451b
jaikaran109 <jaik05581@gmail.com>
2026-08-26 01:46:18 +0530

    Create code.java

---
+++ SomeExtraQues/Recursion/Medium/LC_Q46_Permutations/code.java
@@ -0,0 +1,30 @@
+// Backtracking use ho rha h to code complex lg skta h , Readme file me explaination h vo check kr lo
+
+class Solution {
+
+    static void helper(int[] nums,List<List<Integer>> ans , List<Integer> ds , boolean[] freq){
+        if(ds.size() == nums.length){
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+
+        for(int i = 0 ; i < nums.length ; i++){
+            if(!freq[i]){
+                freq[i] = true;
+                ds.add(nums[i]);
+                helper(nums,ans,ds,freq);
+                ds.remove(ds.size() - 1);
+                freq[i] = false;
+            }
+        }
+
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+        boolean[] freq = new boolean[nums.length];
+        helper(nums,ans,ds,freq);
+        return  ans;
+    }
+}
